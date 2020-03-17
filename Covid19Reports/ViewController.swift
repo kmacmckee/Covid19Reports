400d07cc7bcab247bf7501143b98d200018239eb
Kobe McKee <kobe.mckee@gmail.com>
2020-03-17 17:21:37 -0600

    Initial Commit

---
+++ Covid19Reports/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19Reports
+//
+//  Created by Kobe McKee on 3/17/20.
+//  Copyright © 2020 Kobe McKee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
